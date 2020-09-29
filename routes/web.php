9c65a7418a77f5dfffbfccc7e08bf7b2623c507c
Bobby Ekaro <iambobbymax@gmail.com>
2020-09-29 09:21:00 +0100

    Redirect to Login Page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('user.dashboard');
 });
 
 Auth::routes();
